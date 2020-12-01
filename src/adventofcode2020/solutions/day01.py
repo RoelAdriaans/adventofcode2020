e69d003bc40969f5f0e886ac9fa3a691d3231174
Roel Adriaans <roel@adriaans.org>
2020-12-01 11:46:44 +0100

    Use math package and remove duplicate code

--- src/adventofcode2020/solutions/day01.py
+++ src/adventofcode2020/solutions/day01.py
@@ -1,25 +1,24 @@
+import math
 from itertools import combinations
 
 from adventofcode2020.utils.abstract import FileReaderSolution
 
 
 class Day01:
-    pass
+    @staticmethod
+    def compute_factor(input_data, n):
+        ints = [int(x) for x in input_data.split("\n")]
+        for x in combinations(ints, n):
+            if sum(x) == 2020:
+                return math.prod(x)
+        return -1
 
 
 class Day01PartA(Day01, FileReaderSolution):
     def solve(self, input_data: str) -> int:
-        ints = [int(x) for x in input_data.split("\n")]
-        for x in combinations(ints, 2):
-            if sum(x) == 2020:
-                return x[0] * x[1]
-        return -1
+        return self.compute_factor(input_data, 2)
 
 
 class Day01PartB(Day01, FileReaderSolution):
     def solve(self, input_data: str) -> int:
-        ints = [int(x) for x in input_data.split("\n")]
-        for x in combinations(ints, 3):
-            if sum(x) == 2020:
-                return x[0] * x[1] * x[2]
-        return -1
+        return self.compute_factor(input_data, 3)
