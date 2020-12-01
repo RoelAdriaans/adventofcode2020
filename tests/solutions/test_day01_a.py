543ed51c1b512986ccd6e3bf73fde407183fc6aa
Roel Adriaans <roel@adriaans.org>
2020-12-01 08:01:36 +0100

    Compute day1

--- tests/solutions/test_day01_a.py
+++ tests/solutions/test_day01_a.py
@@ -4,14 +4,6 @@ from adventofcode2020.solutions.day01 import Day01PartA
 
 
 class TestDay01PartA:
-    @pytest.mark.skip("This code is not yet implemented.")
-    @pytest.mark.parametrize(("input_data", "expected_result"), [("", ""), ("", "")])
-    def test_day01a_solve(self, input_data, expected_result):
-        solution = Day01PartA()
-        result = solution.solve(input_data)
-        assert result == expected_result
-
-    @pytest.mark.skip("This code is not yet implemented.")
     def test_day01a_data(self):
         """ Result we got when we did the real solution """
         solution = Day01PartA()
