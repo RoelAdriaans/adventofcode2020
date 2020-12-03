a897434042cf7241c218438c179c6932f8e1dc78
Roel Adriaans <roel@adriaans.org>
2020-12-03 06:43:34 +0100

    Clean it up a bit

--- src/adventofcode2020/solutions/day03.py
+++ src/adventofcode2020/solutions/day03.py
@@ -3,43 +3,27 @@ import math
 
 
 class Day03:
-    pass
+    def solve_options(self, input_data: str, right: int, down: int) -> int:
+        count = 0
+        pos = 0
+        lines = input_data.split("\n")
+        for idx in range(0, len(lines), down):
+            line = lines[idx]
+            pos_mod = pos % len(line)
+
+            if line[pos_mod] == "#":
+                count += 1
+
+            pos += right
+        return count
 
 
 class Day03PartA(Day03, FileReaderSolution):
     def solve(self, input_data: str) -> int:
-        count = 0
-        pos = 0
-        lines = input_data.split("\n")
-        for line in lines:
-            # Going down
-            pos_mod = pos % len(line)
-            char_at_los = line[pos_mod]
-            if char_at_los == "#":
-                count += 1
-            pos += 3
-        return count
+        return self.solve_options(input_data, 3, 1)
 
 
 class Day03PartB(Day03, FileReaderSolution):
-    def solve_options(self, input_data, right, down):
-        count = 0
-        pos = 0
-        line_num = 1
-        lines = input_data.split("\n")
-        for line in lines:
-            line_num += 1
-            if down == 2 and line_num % 2:
-                continue
-
-            # Going down
-            pos_mod = pos % len(line)
-            char_at_los = line[pos_mod]
-            if char_at_los == "#":
-                count += 1
-            pos += right
-        return count
-
     def solve(self, input_data: str) -> int:
         numbers = [
             self.solve_options(input_data, 1, 1),
--- tests/solutions/test_day03_a.py
+++ tests/solutions/test_day03_a.py
@@ -1,19 +1,10 @@
-import pytest
-
 from adventofcode2020.solutions.day03 import Day03PartA
 
 
 class TestDay03PartA:
-    @pytest.mark.parametrize(("input_data", "expected_result"), [("", ""), ("", "")])
-    def test_day03a_solve(self, input_data, expected_result):
-        solution = Day03PartA()
-        result = solution.solve(input_data)
-        assert result == expected_result
-
-
     def test_day03a_data(self):
         """ Result we got when we did the real solution """
         solution = Day03PartA()
         res = solution("day_03/day03.txt")
         assert res != 105
-        assert res == 0
+        assert res == 151
