c3da7c37b319340487746c0a4942bdb149616c6e
Roel Adriaans <roel@adriaans.org>
2020-12-03 06:17:24 +0100

    Day 03, speed run

---
+++ tests/solutions/test_day03_b.py
@@ -0,0 +1,13 @@
+import pytest
+
+from adventofcode2020.solutions.day03 import Day03PartB
+
+
+class TestDay03PartB:
+    def test_day03b_data(self):
+        """ Result we got when we did the real solution """
+        solution = Day03PartB()
+        res = solution("day_03/day03.txt")
+        assert res != 294
+        assert res != 13163297103
+        assert res == 7540141059
