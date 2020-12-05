59109744159ab58f1c1bcd80ee149f4a6503212a
Roel Adriaans <roel@adriaans.org>
2020-12-05 16:38:52 +0100

    Solve day 5, part b

--- src/adventofcode2020/solutions/day05.py
+++ src/adventofcode2020/solutions/day05.py
@@ -1,4 +1,22 @@
+from __future__ import annotations
+
+from collections import defaultdict
+from typing import Dict, NamedTuple
+
 from adventofcode2020.utils.abstract import FileReaderSolution
+
+
+class BoardingTicket(NamedTuple):
+    row: int
+    col: int
+    ticketnumber: str
+
+    @staticmethod
+    def from_str(input_str) -> BoardingTicket:
+        row = Day05.compute_position_row(input_str)
+        col = Day05.compute_position_col(input_str)
+
+        return BoardingTicket(row=row, col=col, ticketnumber=input_str)
 
 
 class Day05:
@@ -55,4 +73,18 @@ class Day05PartA(Day05, FileReaderSolution):
 
 class Day05PartB(Day05, FileReaderSolution):
     def solve(self, input_data: str) -> int:
-        raise NotImplementedError
+        tickets: Dict[int, Dict] = defaultdict(dict)
+        for boarding_str in input_data.splitlines():
+            boarding_ticket = BoardingTicket.from_str(boarding_str)
+            tickets[boarding_ticket.row][boarding_ticket.col] = boarding_ticket
+
+        # My ticket is missing; SOO there must be:
+        # TMT
+        # Where M is mine, and T = ticket found
+        for row in tickets:
+            for col in tickets:
+                # Check if our the seat next to us is empty, but the set after that is
+                # filled
+                if not tickets[row].get(col + 1) and tickets[row].get(col + 2):
+                    return row * 8 + col + 1
+        return -1
--- tests/solutions/test_day05_b.py
+++ tests/solutions/test_day05_b.py
@@ -1,19 +1,9 @@
-import pytest
-
 from adventofcode2020.solutions.day05 import Day05PartB
 
 
 class TestDay05PartB:
-    @pytest.mark.skip("This code is not yet implemented.")
-    @pytest.mark.parametrize(("input_data", "expected_result"), [("", ""), ("", "")])
-    def test_day05b_solve(self, input_data, expected_result):
-        solution = Day05PartB()
-        result = solution.solve(input_data)
-        assert result == expected_result
-
-    @pytest.mark.skip("This code is not yet implemented.")
     def test_day05b_data(self):
         """ Result we got when we did the real solution """
         solution = Day05PartB()
         res = solution("day_05/day05.txt")
-        assert res == 0
+        assert res == 612
